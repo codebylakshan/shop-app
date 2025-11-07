ccbb406a6f5f7d5293286c528b251e607ba4c093
codebylakshan <dlakshan09@gmail.com>
2025-11-07 23:12:03 +0530

    DB connection

--- src/Products.tsx
+++ src/Products.tsx
@@ -1,52 +1,38 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import ProductCard from "./ProductCard";
 
-let products = [
-  {
-    id: 1,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "$35",
-    color: "Black",
-  },
-  {
-    id: 2,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-02.jpg",
-    imageAlt: "Front of men's Basic Tee in white.",
-    price: "$35",
-    color: "Aspen White",
-  },
-  {
-    id: 3,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-03.jpg",
-    imageAlt: "Front of men's Basic Tee in dark gray.",
-    price: "$35",
-    color: "Charcoal",
-  },
-  {
-    id: 4,
-    name: "Artwork Tee",
-    href: "#",
-    imageSrc: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg",
-    imageAlt: "Front of men's Artwork Tee in peach with white and brown dots forming an isometric cube.",
-    price: "$35",
-    color: "Iso Dots",
-  },
-];
+type Product = {
+  id: number;
+  name: string;
+  href: string;
+  imageSrc: string;
+  imageAlt: string;
+  price: string;
+  color: string;
+};
 
 function Products() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    console.log("Fetching products...");
+    axios
+      .get("http://localhost:3000/api/products")
+      .then((res) => {console.log(res); setProducts(res.data)})
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+
   return (
     <>
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Customers also purchased
+          </h2>
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
+            {Array.isArray(products) && products.map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
           </div>
